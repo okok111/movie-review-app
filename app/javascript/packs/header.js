07e4df7d940ec141c9eb5d442f8896658628544f
okok111 <1for.rails@gmail.com>
2023-07-05 18:16:50 +0900

    js微修正

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,8 +1,8 @@
 const header = document.querySelector('header');
-let prevY = window.pageYOffset; // 前回のスクロール位置を初期化
+let prevY = window.scrollY; // 前回のスクロール位置を初期化
 
 window.addEventListener('scroll', () => {
-  const currentY = window.pageYOffset; // 現在のスクロール位置を取得
+  const currentY = window.scrollY; // 現在のスクロール位置を取得
   if (currentY < prevY) { // 上にスクロールしている場合
     header.classList.remove('hidden'); // hiddenクラスを削除して表示する
   } else { // 下にスクロールしている場合
